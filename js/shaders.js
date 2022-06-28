3803c87384073f995a2cd593959030a9b7bf0b6f
Oscar <osaharoy@gmail.com>
2022-06-28 22:42:03 +0100

    changed shader

--- js/shaders.js
+++ js/shaders.js
@@ -5,8 +5,7 @@ const fragmentShader = `
 
 // ====================================================================================
 
-// http://www.pouet.net/prod.php?which=57245
-// If you intend to reuse this shader, please add credits to 'Danilo Guanabara'
+// remix of http://www.pouet.net/prod.php?which=57245 by Danilo Guanabara
 
 #define t uTime
 #define r uResolution.xy
@@ -15,20 +14,24 @@ uniform float uTime;
 uniform vec2 uResolution;
 
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord ){
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+
 	vec3 c;
-	float l,z=t;
+	float l, z = t;
+
+    vec2 p=fragCoord.xy/r;
+    vec2 uvs = p;
+    p-=.5;
+    p.x *= r.x/r.y;
+    l=length(p);
+
 	for(int i=0;i<3;i++) {
-		vec2 uv,p=fragCoord.xy/r;
-		uv=p;
-		p-=.5;
-		p.x*=r.x/r.y;
-		z+=.07;
-		l=length(p);
-		uv+=p/l*(sin(z)+1.)*abs(sin(l*9.-z-z));
-		c[i]=.01/length(mod(uv,1.)-.5);
+		z+=.05;
+		vec2 uv = uvs + p/l*(sin(z-l*0.2)+1.3) * sin(sin(l*10.)+z*2.);
+		c[i]=.02/length(mod(uv,1.)-.5)/l;
 	}
-	fragColor=vec4(c/l,t);
+
+	fragColor = vec4(c,1.);
 }
 
 
